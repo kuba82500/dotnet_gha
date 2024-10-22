496761be8fafcdcb1275b12b260adead905670bb
Jakub <kuba82500@gmail.com>
2024-10-22 18:55:17 +0200

    MORE CHANGES
    move gitignore file to top folder and add more logs

--- actions/console-app/Program.cs
+++ actions/console-app/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("Hello, World!");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("SUCCESS! You build and run your app in github actions");
